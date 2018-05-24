4a5aed4159bd185dd4c9b5128e405e7fcb8d1283
kris <kristolo11@gmail.com>
2018-05-25 01:00:19 +0200

    bugs fix in task 3

--- task3.py
+++ task3.py
@@ -21,10 +21,25 @@ def map_reduce(map_func, reduce_func, num_workers):
     def map_function(process_name, tasks, results):
         print('[%s] evaluation routine starts' % process_name)
 
+        # while True:
+        #     try:
+        #         word = tasks.get()
+        #     except queue.Empty:
+        #         results.put(-1)
+        #         break
+        #     else:
+        #         results.put(map_func(word[0], word[1]))
+
+        # while not tasks.empty():
+        #     word = tasks.get()
+        #     results.put(map_func(word[0], word[1]))
+        #
+        # results.put(-1)
+
         while True:
-            try:
-                word = tasks.get()
-            except queue.Empty:
+            word = tasks.get()
+            if word == -1:
+
                 results.put(-1)
                 break
             else:
@@ -35,21 +50,36 @@ def map_reduce(map_func, reduce_func, num_workers):
     def reduce_function(process_name, tasks, results):
         print('[%s] evaluation routine starts' % process_name)
 
+        # while True:
+        #     try:
+        #         map_object = tasks.get()
+        #     except queue.Empty:
+        #         results.put(-1)
+        #         break
+        #     else:
+        #         results.put(reduce_func(map_object[0], sum(map_object[1])))
+
+        # while not tasks.empty():
+        #     map_object = tasks.get()
+        #     results.put(reduce_func(map_object[0], sum(map_object[1])))
+        #
+        # results.put(-1)
+
         while True:
-            try:
-                map_object = tasks.get()
-            except queue.Empty:
+            map_object = tasks.get()
+            if map_object == -1:
+
                 results.put(-1)
                 break
             else:
-                results.put(reduce_func(map_object[0], sum(map_object[1])))
+                results.put(reduce_func(map_object[0], map_object[1]))
 
         return
 
     def map_reduce_func(word_list):
         list_of_mappings = []
 
-        if __name__ == "__main__":
+        if __name__ == "task3":
             manager = multiprocessing.Manager()
             tasks = manager.Queue()
             results = manager.Queue()
@@ -67,22 +97,26 @@ def map_reduce(map_func, reduce_func, num_workers):
 
             num_finished_processes = 0
 
+            for i in range(num_workers):
+                tasks.put(-1)
+
             while True:
-                new_result = results.get()
+                if not results.empty():
+                    new_result = results.get()
 
-                if new_result == -1:
-                    num_finished_processes += 1
+                    if new_result == -1:
+                        num_finished_processes += 1
 
-                    if num_finished_processes == num_workers:
-                        break
-                else:
-                    list_of_mappings.append(new_result)
+                        if num_finished_processes == num_workers:
+                            break
+                    else:
+                        list_of_mappings.append(new_result)
 
         list_of_mappings = sorted(list_of_mappings, key=lambda item: item[0])
         grouped_mapings = group_mappings(list_of_mappings)
         mapped_and_reduced = []
 
-        if __name__ == "__main__":
+        if __name__ == "task3":
             manager = multiprocessing.Manager()
             tasks = manager.Queue()
             results = manager.Queue()
@@ -95,24 +129,25 @@ def map_reduce(map_func, reduce_func, num_workers):
                 processes.append(new_process)
                 new_process.start()
 
-            for word in word_list:
-                tasks.put((word, 1))
-
             for mapping in grouped_mapings:
                 tasks.put(mapping)
 
             num_finished_processes = 0
 
+            for i in range(num_workers):
+                tasks.put(-1)
+
             while True:
-                new_result = results.get()
+                if not results.empty():
+                    new_result = results.get()
 
-                if new_result == -1:
-                    num_finished_processes += 1
+                    if new_result == -1:
+                        num_finished_processes += 1
 
-                    if num_finished_processes == num_workers:
-                        break
-                else:
-                    list_of_mappings.append(new_result)
+                        if num_finished_processes == num_workers:
+                            break
+                    else:
+                        mapped_and_reduced.append(new_result)
 
         return mapped_and_reduced
 
--- task4.py
+++ task4.py
@@ -27,6 +27,7 @@ def main():
     map_reduce_function = map_reduce(map_function, reduce_function, 4)
     word_count = map_reduce_function(word_list)
     word_count = sorted(word_count, key=lambda element: element[1])
+    word_count.reverse()
 
     for item in word_count[:20]:
         print(item)
